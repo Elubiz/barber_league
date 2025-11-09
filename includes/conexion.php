331980f7d82f69a695bbc8d55c1f39ad9c57f987
Elian Navarro <elian@gmail.com>
2025-11-09 10:03:01 -0500

    ...

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,17 +1,14 @@
 <?php
-$host = "localhost";   // Servidor local
-$user = "root";        // Usuario por defecto de XAMPP
-$pass = "";            // Contraseña vacía por defecto
-$db   = "barberia_db"; // Nombre de la base de datos
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "barberia_db";
 
-// Crear conexión
-$conexion = new mysqli($host, $user, $pass, $db);
+$conexion = mysqli_connect($host, $user, $pass, $db);
 
-// Verificar conexión
-if ($conexion->connect_error) {
-    die("Error de conexión: " . $conexion->connect_error);
+if (!$conexion) {
+    die("Error de conexión: " . mysqli_connect_error());
 }
 
-
-$conexion->set_charset("utf8");
+mysqli_set_charset($conexion, "utf8");
 ?>
